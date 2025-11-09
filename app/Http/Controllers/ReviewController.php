3bd6f19404bdeb04873faf6b9cdd4a7f740e4de2
AmadeusIan <amadeusiangunadi19@gmail.com>
2025-11-10 05:48:38 +0700

    Commit coba

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,19 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Review;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ReviewController extends Controller
 {
 
-    public function index(){
+    public function index(Request $request){
    
-
+        if($request->has('search')){
+            $products = Product::where('name', 'like', '%'.$request->search.'%')->get();
+        } else{
         $reviews = Review::oldest()
                 ->with(['user','product']) //buat ambil data user dan product juga
                 ->take(3)
                 ->get();
                 
             $products = \App\Models\Product::with('reviews')->paginate(6);
+        }
 
 
             return view('homepage', [
